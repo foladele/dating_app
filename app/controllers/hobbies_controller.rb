715a0008d4e302025ab69c5f4c6ae0484ea8fff4
Faith Oladele <faitholadele@gmail.com>
2016-06-01 09:49:32 -0600

    adding gem

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,8 +1,23 @@
 class HobbiesController < ApplicationController
+before_action :person
+before_action :hobby, only: [:show, :edit, :update, :destroy]
+
   def index
+  	@hobbies = @person.hobbies
   end
 
   def new
+  	@hobby = Hobby.new
+  end
+
+  def create
+  	@hobby = @person.hobbies.new(hobby_params)
+  	if @hobby.save
+  		flash[:success] = '#{@hobby.name} has been created!'
+  		redirect_to person_hobby_path(@person, @hobby)
+  	else
+  		render :new
+  	end
   end
 
   def show
@@ -10,4 +25,34 @@ class HobbiesController < ApplicationController
 
   def edit
   end
+
+  def update
+  	if @hobby.update(hobby_params)
+  		flash[:success] = "hobby has been updated!"
+  		redirect_to person_hobby_path(@person, @hobby)
+  	else
+  		render :edit
+  	end
+  end
+
+
+  def destroy
+  	@hobby.destroy
+  	redirect_to person_hobbies_path(@person)
+  end
+
+  private
+
+  	def hobby_params
+  		params.require(:hobby).permit(:description)
+  	end
+
+  	def hobby
+  		@hobby = @person.hobbies.find(params[:id])
+  	end
+
+  	def person
+  	
+  		@person = Person.find(params[:person_id])
+  	end
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,6 +13,7 @@ class PeopleController < ApplicationController
   def show
   	@person = Person.find(params[:id])
     @user = current_user
+    @person.address
   end
 
   def new
